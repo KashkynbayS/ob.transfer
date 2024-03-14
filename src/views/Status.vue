3d23607217c65d6e3b1d37d0fc508999dff26aaf
Manar Kurmanov <manar.kurmanov@kmf.kz>
2024-03-14 20:37:27 +0500

    images fix

--- src/views/Status.vue
+++ src/views/Status.vue
@@ -4,7 +4,7 @@ import { computed, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 
 import Error from '@/assets/images/error.png'
-import Info from '@/assets/images/info.svg'
+import Info from '@/assets/images/info.png'
 import Success from '@/assets/images/success.png'
 
 import StatusPageLayout from '@/layouts/StatusPageLayout.vue'
@@ -38,12 +38,12 @@ onMounted(() => {
 </script>
 
 <template>
-	<StatusPageLayout :image="image" :title="statusStore.title" title-width="80%" :text="statusStore.description">
-		<template #footer>
-			<Button v-for="action in statusStore.actions" id="status-action" :key="action.url"
-				:type="action.type === 'secondary' ? 'ghost' : 'primary'" @click="handleClick(action)">
-				{{ action.title }}
-			</Button>
-		</template>
-	</StatusPageLayout>
+<StatusPageLayout :image="image" :title="statusStore.title" title-width="80%" :text="statusStore.description">
+	<template #footer>
+		<Button v-for="action in statusStore.actions" id="status-action" :key="action.url"
+			:type="action.type === 'secondary' ? 'ghost' : 'primary'" @click="handleClick(action)">
+			{{ action.title }}
+		</Button>
+	</template>
+</StatusPageLayout>
 </template>
