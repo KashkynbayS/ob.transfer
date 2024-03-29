7bd7978614f5022cb5fbc3d179b19b34193cea02
Sultan <s.kashkynbay@strongte.am>
2024-03-29 06:34:20 +0500

    Updated success page

--- src/layouts/StatusPageLayout.vue
+++ src/layouts/StatusPageLayout.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+import { Cell, CellGroup, Switch } from '@ui-kit/ui-kit';
+
+import { addToFavorites, removeFromFavorites } from '@/services/frequent.service';
+
+import { useStatusStore } from '@/stores/status.ts';
+import { useApplicationIDStore } from '@/stores/useApplicationIDStore';
+
 import PageTemplate from '@/layouts/PageTemplate.vue';
 
 import TransferDetailsBottomSheet from '@/components/DetailsBottomSheet.vue';
+
+const applicationIDStore = useApplicationIDStore()
+const statusStore = useStatusStore()
 
 const props = withDefaults(
 	defineProps<{
@@ -18,13 +30,59 @@ const props = withDefaults(
 		titleWidth: '100%'
 	}
 )
+
+const cellSwitch = ref(false)
+
+const handleAddToFavourites = async (value: boolean) => {
+	const applicationID = applicationIDStore.applicationID
+
+	if (!applicationID) {
+		return;
+	}
+
+	if (value) {
+		await onAddToFavourites(applicationID)
+	} else {
+		await onRemoveFromFavourites(applicationID)
+	}
+}
+
+const onAddToFavourites = async (applicationID: string) => {
+	try {
+		const addedItem = await addToFavorites(applicationID)
+		console.log('Перевод успешно добавлен в избранное:', addedItem);
+	} catch (error) {
+		console.error('Ошибка при добавлении в избранное:', error);
+	}
+}
+
+const onRemoveFromFavourites = async (applicationID: string) => {
+	try {
+		const itemToRemove = await removeFromFavorites(applicationID)
+		console.log('Перевод успешно добавлен в избранное:', itemToRemove);
+	} catch (error) {
+		console.error('Ошибка при добавлении в избранное:', error);
+	}
+}
 </script>
 
 <template>
 	<PageTemplate>
-		<img :src="props.image" :alt="props.title" :class="props.imageSize" />
-		<h3>{{ props.title }}</h3>
-		<p class="text-low-contrast">{{ props.text }}</p>
+		<section class="content-body">
+			<img :src="props.image" :alt="props.title" :class="props.imageSize" />
+			<h3>{{ props.title }}</h3>
+			<p class="text-low-contrast">{{ props.text }}</p>
+
+			<CellGroup v-if="statusStore.class == 'success'" type="island" class="success__favourite">
+				<Cell rightType="controls">
+					<template #title> {{ $t('SUCCESS.ADD_TO_FAVOURITES') }} </template>
+
+					<template #right>
+						<Switch id="cell-switch" v-model="cellSwitch" :onUpdate:modelValue="handleAddToFavourites" />
+					</template>
+				</Cell>
+			</CellGroup>
+		</section>
 		<template #footer>
 			<slot name="footer" />
 		</template>
@@ -34,14 +92,21 @@ const props = withDefaults(
 </template>
 
 <style scoped lang="scss">
+.content-body {
+	flex-grow: 1;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: var(--space-4);
+}
+
 img {
 	height: auto;
-	margin-bottom: var(--space-6);
-	padding: 24px;
-	margin-top: 72px;
 
 	&.l {
-		width: 343px;
+		width: 232px;
 	}
 
 	&.m {
@@ -50,15 +115,18 @@ img {
 }
 
 h3 {
-	margin-bottom: var(--space-4);
+	font-size: var(--font-size-heading-3);
 	text-align: center;
 	max-width: v-bind(titleWidth);
 }
 
 p {
-	flex-grow: 1;
-	margin-bottom: var(--space-6);
 	max-width: v-bind(textWidth);
 	text-align: center;
+}
+
+.success__favourite {
+	width: 100%;
+	padding: 0;
 }
 </style>
