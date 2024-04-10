6925d6835d2fea1c3b6f11d4616dc2ea047e88f6
Manar Kurmanov <manar.kurmanov@kmf.kz>
2024-04-10 11:49:21 +0500

    [DBO-889] token expired modal update

--- src/api/interceptors/request-token-expired.ts
+++ src/api/interceptors/request-token-expired.ts
@@ -1,10 +1,11 @@
+
 import { InternalAxiosRequestConfig } from "axios"
 import { jwtDecode } from "jwt-decode"
 
 import { tokenExpired } from "@ui-kit/events"
 
-import { useAlertsStore } from "@/stores/alerts"
-import { AlertType } from "@/types/alert"
+import { useAlertsStore } from "@/stores"
+import { AlertType } from "@/types"
 
 const EXPIRED_ALERT: AlertType = {
     show: true,
@@ -21,20 +22,32 @@ const EXPIRED_ALERT: AlertType = {
     ]
 }
 
+const showExpiredAlert = (config: InternalAxiosRequestConfig<any>) => {
+    const alertStore = useAlertsStore()
+    const abortController = new AbortController()
+
+    config.signal = abortController.signal
+
+    abortController.abort()
+    alertStore.showErrorAlert(EXPIRED_ALERT)
+}
+
 export const requestTokenExpiredInterceptor = (
     config: InternalAxiosRequestConfig<any>
 ) => {
-    if (!config || !config.headers.Authorization) return config
+    if (!sessionStorage.accessToken) {
+        showExpiredAlert(config)
+    }
 
-    const abortController = new AbortController()
-    const alertStore = useAlertsStore()
-    const decoded = jwtDecode(sessionStorage.accessToken) as any
-
-    if (Date.now() > decoded.exp * 1000) {
-        config.signal = abortController.signal
-        abortController.abort()
-        alertStore.showErrorAlert(EXPIRED_ALERT)
+    try {
+        const decoded = jwtDecode(sessionStorage.accessToken) as any
+        if (Date.now() > decoded.exp * 1000) {
+            showExpiredAlert(config)
+        }
+    } catch {
+        showExpiredAlert(config)
     }
 
     return config
 }
+
