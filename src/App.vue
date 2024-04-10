5cd8b08b0ab20f28ed2154ec4e48e8baf275f0d8
Manar Kurmanov <manar.kurmanov@kmf.kz>
2024-04-10 10:47:50 +0500

    [DBO-889] token expire added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
 
-import { HttpLogger, LoaderPage } from '@ui-kit/ui-kit';
+import { HttpLogger, LoaderPage, Modal } from '@ui-kit/ui-kit';
 
 import TargetBlankModal from '@/components/TargetBlankModal.vue';
 
@@ -9,12 +9,24 @@ import { useAlertsStore } from '@/stores/alerts.ts';
 import { useLoadingStore } from '@/stores/loading.ts';
 
 import { isDebug } from '@/utils';
-
+import { ref, watch } from 'vue';
 
 const loadingStore = useLoadingStore()
 const alertsStore = useAlertsStore()
 
 const { errorAlert } = storeToRefs(alertsStore)
+const errorModalRef = ref<InstanceType<typeof Modal> | null>()
+
+watch(
+	() => errorAlert.value.show,
+	(val) => {
+		if (val) {
+			errorModalRef.value?.open()
+		} else {
+			errorModalRef.value?.close()
+		}
+	}
+)
 
 </script>
 
@@ -34,10 +46,10 @@ const { errorAlert } = storeToRefs(alertsStore)
 		<http-logger v-if="isDebug"></http-logger>
 	</transition>
 
-	<modal ref="errorModalRef" :actions="errorAlert.actions">
+	<Modal ref="errorModalRef" :actions="errorAlert.actions">
 		<template #title>{{ errorAlert.title }}</template>
 		<template #body>{{ errorAlert.description }}</template>
-	</modal>
+	</Modal>
 
 	<TargetBlankModal />
 </main>
--- src/api/api.instance.ts
+++ src/api/api.instance.ts
@@ -1,65 +1,22 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'; // Define the type for your token
+import axios from 'axios';
+
+import { tokenExpired } from '@ui-kit/events';
+
+import { useLoadingStore } from '@/stores/loading';
+
 import { requestTokenExpiredInterceptor } from './interceptors/request-token-expired';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL || 'https://dev-api.kmf.kz'
 const LS_TOKEN_KEY = 'accessToken'
 
-interface CustomAxiosRequestConfig extends AxiosRequestConfig {
-	_retry?: boolean
-	retryCount?: number
-}
-
-if (import.meta.env.VITE_ENVIRONMENT === 'local') {
-	sessionStorage.setItem(
-		LS_TOKEN_KEY,
-		'eyJhbGciOiJSUzUxMiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJOdkpSQ1VtVkd5UnZBbTF3aFcwZWxleEFVSnJUNFpCeTBRTFI3YXBZQV9vIn0.eyJleHAiOjE3MTIyMjYxMjYsImlhdCI6MTcxMjIyNTIyNiwianRpIjoiMWQ1ZDIxOGMtYjZhZS00OGFiLWFiZGQtZGU3Yzg0ZWYwY2YyIiwiaXNzIjoiaHR0cHM6Ly9pZC1kZXYua21mLmt6L3JlYWxtcy9vbmxpbmUtYmFuayIsInN1YiI6IjQyNmIwZTQyLTAxMjktNDY1MC05N2ZlLTUxZDI2OWE5ODVmNCIsInR5cCI6IkJlYXJlciIsImF6cCI6ImFjY291bnQiLCJzZXNzaW9uX3N0YXRlIjoiYzMzMDIzNDAtZmFjMy00MGY2LWE1YjctNzE3OWZlYjc2NjNkIiwic2NvcGUiOiJwaG9uZSIsInNpZCI6ImMzMzAyMzQwLWZhYzMtNDBmNi1hNWI3LTcxNzlmZWI3NjYzZCIsInBlcnNvbmFsIjp7InByb2ZpbGUiOnsiZmlybUlkIjoiMTciLCJicmFuY2hJZCI6IjE0MyIsIm5hbWUiOiLQmNCh0JzQkNCY0Jsg0JguIiwicGhvbmUiOiIrNzc3NjY2NjU5NDciLCJpaW4iOiIwMTExMTQ1MDA1MjIiLCJhdmF0YXIiOiIzNGIwNmU5Yi03NzI4LTQzZmMtOTg5Mi03MjEyYTMyYTA5NTQiLCJzY29wZSI6WyJpbnRlcm5hbCIsImJhc2ljIl19LCJjb2x2aXIiOnsiaWQiOjE0MTEzNzc5LCJkZXBJZCI6NzMzLCJjb2RlIjoiMDAwMDAxMzU5OTI0In0sInR5cGVJZCI6MX19.YK9pwzA5FxrnKxcyohy2Dur9iVaWxIIpFjjawxHhgbbZM2lf2zigEbEr59jquZ0J1FDz9ePoFlNj0UHRM__9tbSoqtDp1uaIonzAFFg84hPV050qkyvwX4mUUsk_XGf4dn60286lj0LbgOmhRVUuEb_Y7uQFg1I55q-psK-RL1V-OkfoNKrcuDNWDSSP-sNO5x9Wdc9_TSIgJu38-HH5HjVqvDKMowvvGg3OJ_EEgpxazEUTjFHyHZ9VcFHlhESZb-5-t7Hef7-p-j22xIrB5IAQaNhBOQJ2HUPBgzLELX9o0CPj_rA0qx_tSrH8eeWCfy85fO0gadjxj_UpdgmLWQ'
-	)
-}
-
-const refreshToken = async () => {
-	const url = `${BASE_URL}/api/bank/sso/internal/customer/authorize`
-	const formData = {
-		phone: '+77081991418',
-		password: 'Qwerty123'
-	}
-
-	return fetch(url, {
-		method: 'POST',
-		body: JSON.stringify(formData)
-	}).then((response) => response.json())
-}
+// if (import.meta.env.VITE_ENVIRONMENT === 'local') {
+// 	sessionStorage.setItem(
+// 		LS_TOKEN_KEY,
+// 		'eyJhbGciOiJSUzUxMiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJOdkpSQ1VtVkd5UnZBbTF3aFcwZWxleEFVSnJUNFpCeTBRTFI3YXBZQV9vIn0.eyJleHAiOjE3MTI1NzEwOTYsImlhdCI6MTcxMjU3MDE5NiwianRpIjoiODg2NDYzMjEtNWEzMy00Mjk2LWJjM2UtNzMyZmZiN2QyNWMyIiwiaXNzIjoiaHR0cHM6Ly9pZC1kZXYua21mLmt6L3JlYWxtcy9vbmxpbmUtYmFuayIsInN1YiI6IjQyNjU2OWU3LWJkNjMtNGE2ZS1hOTRkLTgzMzA0ZjVkNDU2NiIsInR5cCI6IkJlYXJlciIsImF6cCI6ImFjY291bnQiLCJzZXNzaW9uX3N0YXRlIjoiNWExZTg1ZDMtZGYwNC00ZWNhLTlhNmYtNzc2YWUzMWY1N2RmIiwic2NvcGUiOiJwaG9uZSIsInNpZCI6IjVhMWU4NWQzLWRmMDQtNGVjYS05YTZmLTc3NmFlMzFmNTdkZiIsInBlcnNvbmFsIjp7InByb2ZpbGUiOnsiZmlybUlkIjoiNyIsImJyYW5jaElkIjoiNzgiLCJuYW1lIjoi0JTQkNCh0KLQkNCdINCgLiIsInBob25lIjoiKzc3NzcyMTY1NjU2IiwiaWluIjoiOTEwNTAzMzAwNTA3IiwiYXZhdGFyIjoiNTJkNjljMDYtYzAxMS00Y2FmLWEyZjEtMjA2NzE3OGIwNmFiIiwic2NvcGUiOlsiaW50ZXJuYWwiLCJiYXNpYyJdfSwiY29sdmlyIjp7ImlkIjoxNjM5MjYsImRlcElkIjo3MzMsImNvZGUiOiIwMDAwMDA1NDc2ODIifSwidHlwZUlkIjoxfX0.pjQT33OKGJJt1bY-OgZebf9GAQS_LwNllHfaEajZybmXTZTHgMnbmjmj9LHoSpUhLrFhQYx5RBkDzORgNREm7Rctpo3YJX3j_PhvLUBE6GoRQDIYewQfmdCkDkk0Zel4tC9fCviES6hNraVjlTHxWy6bJsZtbdwH321LENmlbRWg6FdT4SNlJL-4ZVDRc77MpeuLdtHjSiCk-TqIuxWfKxE6Cm2f2czpiGkYBnObRDboM3i7axdzURs8wB3v_GnflSzVnXHgseQMmvR5yd5Gdctgu7MYCrmUJ7_-Iz77M_4-tQIxBWg6roSape5jyJwb3NnzZMDtR5-c1CGPz9VQzA'
+// 	)
+// }
 
 const getAccessToken = (): string => `Bearer ${sessionStorage.getItem(LS_TOKEN_KEY) || ''}`
-
-type ErrorHandler = (error: AxiosError, axiosInstance: AxiosInstance) => Promise<AxiosResponse | void>
-const handleTokenExpiry: ErrorHandler = async (error, axiosInstance) => {
-	if (import.meta.env.VITE_ENVIRONMENT !== 'local') {
-		return
-	}
-
-	const originalRequest = error.config as CustomAxiosRequestConfig
-	if (originalRequest && originalRequest.headers && error.response && error.response.status === 401) {
-		originalRequest._retry = true
-		originalRequest.retryCount = originalRequest.retryCount || 0
-
-		if (originalRequest.retryCount >= 2) {
-			return Promise.reject(error)
-		}
-
-		originalRequest.retryCount++
-
-		try {
-			const token = await refreshToken()
-			sessionStorage.setItem(LS_TOKEN_KEY, (token as any)?.data.access)
-			return axiosInstance.request(originalRequest)
-		} catch (e) {
-			console.log('refresh token error', e)
-			return Promise.reject(error)
-		}
-	}
-
-	return Promise.reject(error)
-}
 
 const language = 'RU'
 
@@ -78,11 +35,21 @@ axiosInstance.interceptors.request.use((config) => {
 })
 
 axiosInstance.interceptors.response.use(
-	(response) => {
-		return response
-	},
+	(response) => response,
+	(error) => {
+		console.error('interceptors error:', error)
 
+		if (error.response?.status === 401 || error.response?.data?.msg?.includes('token')) {
+			console.info('token error', error)
+			tokenExpired()
+		}
+
+		useLoadingStore().setLoading(false)
+
+		return Promise.reject(error)
+	}
 )
+
 
 axiosInstance.interceptors.request.use(requestTokenExpiredInterceptor)
 
